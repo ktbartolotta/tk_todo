5a5d6700653cdd6c122fcf3dcaff84c35f6dab1c
Keith Bartolotta <ktbartolotta@gmail.com>
2016-06-26 15:39:26 -0400

    Starting to build out mvvm situation

--- main.py
+++ main.py
@@ -7,20 +7,13 @@ import collections
 #     'ListItem', ['id', 'order', 'status', 'title', 'description'])
 
 
-todo_list = [
-    ListItem(1, 2, 'Done', 'go shopping', 'get items at grocery store.'),
-    ListItem(2, 3, 'In Progress', 'wash car', 'complete washing the car.'),
-    ListItem(3, 1, 'Done', 'feed cat', 'feed the cat at 7:00 pm.')
-]
-
-
 # class ItemFrame(ttk.Frame):
 
 #     def __init__(self, parent, item=None):
 #         ttk.Frame.__init__(self, parent)
 
 #         self.pack()
-
+root = Tkinter.Tk()
 
 class ItemModel(object):
 
@@ -28,6 +21,21 @@ class ItemModel(object):
             self, id=None, order=None, status=None, title='', description=''):
         self.id = Tkinter.IntVar()
         self.id.set(id)
+        self.order = Tkinter.IntVar()
+        self.order.set(order)
+        self.status = Tkinter.StringVar()
+        self.status.set(status)
+        self.title = Tkinter.StringVar()
+        self.title.set(title)
+        self.description = Tkinter.StringVar()
+        self.description.set(description)
+
+
+todo_list = [
+    ItemModel(1, 2, 'Done', 'go shopping', 'get items at grocery store.'),
+    ItemModel(2, 3, 'In Progress', 'wash car', 'complete washing the car.'),
+    ItemModel(3, 1, 'Done', 'feed cat', 'feed the cat at 7:00 pm.')
+]
 
 
 class ListFrame(ttk.Frame):
@@ -45,9 +53,9 @@ class ListFrame(ttk.Frame):
 
         self._add_item(item)
         row = len(self.item_list) - 1
-        ttk.Label(self, text=item.status).grid(row=row, column=0)
-        ttk.Label(self, text=item.title).grid(row=row, column=1)
-        ttk.Label(self, text=item.description).grid(row=row, column=2)
+        ttk.Label(self, textvariable=item.status).grid(row=row, column=0)
+        ttk.Label(self, textvariable=item.title).grid(row=row, column=1)
+        ttk.Label(self, textvariable=item.description).grid(row=row, column=2)
         # ttk.Label(self, text=item['status']).grid(row=row, column=0)
         # ttk.Label(self, text=item['title']).grid(row=row, column=1)
         # ttk.Label(self, text=item['description']).grid(row=row, column=2)
@@ -93,18 +101,16 @@ class ToDoListApp(ttk.Frame):
 
     def add_item(self):
 
-        item = ListItem(
-            '', '',
-            'In Progress',
-            self.title_entry.get(),
-            self.desc_entry.get())
+        item = ItemModel(
+            status='In Progress',
+            title=self.title_entry.get(),
+            description=self.desc_entry.get())
 
         self.list_frame.add_item(item)
 
 
 if __name__ == '__main__':
 
-    root = Tkinter.Tk()
     root.geometry('{x}x{y}+0+0'.format(x=450, y=1000))
     app = ToDoListApp(parent=root, item_list=todo_list)
     root.mainloop()
